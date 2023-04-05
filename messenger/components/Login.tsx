4c9ce6015275894972a4a702be7c65701b561cf3
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-04-05 21:36:28 +0800

    last

--- messenger/components/Login.tsx
+++ messenger/components/Login.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import React from "react";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Image
+        src="https://links.papareact.com/2i6"
+        alt="logo"
+        width={300}
+        height={300}
+      />
+      <button>Sign in to use messenger</button>
+    </div>
+  );
 }
